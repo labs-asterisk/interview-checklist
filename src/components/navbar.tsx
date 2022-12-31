f82e71146e5ecade6bb2bafe8e52dd12da657638
Somesh Kar <somesh.kar@gmail.com>
2022-12-31 12:02:07 +0530

    Stylize navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,16 +1,46 @@
-import { Flex } from "@chakra-ui/react";
+import Link from "next/link";
+
+import { Flex, Icon, Text } from "@chakra-ui/react";
+
+import { MdHome, MdQuestionAnswer, MdNoteAlt } from "react-icons/md";
 
 const navItems = [
   {
     title: "Home",
+    href: "/",
+    icon: MdHome,
+  },
+  {
+    title: "Problems",
+    href: "/problems",
+    icon: MdNoteAlt,
   },
 ];
 
 const Navbar: React.FC = () => {
   return (
-    <Flex flexDir="column" bg={"linear-gradient(to right, #222B3E, #0F1B32)"}>
-      <div>hello</div>
-      <div>hi</div>
+    <Flex
+      minH="100%"
+      flexDir="column"
+      bg={"linear-gradient(to right, #222B3E, #0F1B32)"}
+      textColor="white"
+      p={10}
+    >
+      {navItems.map(({ title, href, icon }) => (
+        <Link href={href}>
+          <Flex
+            mb={10}
+            p={10}
+            borderRadius="5px"
+            alignItems="center"
+            transition="all 0.2s ease-in-out"
+            _hover={{ bg: "rgba(221,226,255, 0.08)" }}
+          >
+            <Icon h={30} w={30} as={icon} />
+            <Text ml={0}>{title}</Text>
+          </Flex>
+        </Link>
+      ))}
     </Flex>
   );
 };
