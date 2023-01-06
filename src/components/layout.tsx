27a559706497c5fceddf39a132b6af62b8d7128a
Somesh Kar <somesh.kar@gmail.com>
2023-01-06 23:50:33 +0530

    Add vercel analytics and change favicon

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,6 +4,8 @@ import GridLoader from "react-spinners/GridLoader";
 import { Grid, GridItem, Flex } from "@chakra-ui/react";
 import { useSession } from "next-auth/react";
 
+import { AnalyticsWrapper } from "./analytics";
+
 import Navbar from "./navbar";
 
 interface LayoutProps {
@@ -23,22 +25,30 @@ const Layout: React.FC<LayoutProps> = ({ title, children }) => {
         <meta name="description" content="Make interview prep great again :P" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Grid maxH="100vh" w="100vw" templateColumns="1fr 5fr" overflowY="hidden">
-        <GridItem minH="100%">
-          <Navbar />
-        </GridItem>
-        <GridItem height="100vh" overflowY="auto">
-          <main>
-            {status === "loading" ? (
-              <Flex minH="100vh" alignItems="center" justifyContent="center">
-                <GridLoader color="#172237" size={20} />
-              </Flex>
-            ) : (
-              children
-            )}
-          </main>
-        </GridItem>
-      </Grid>
+      <main>
+        <Grid
+          maxH="100vh"
+          w="100vw"
+          templateColumns="1fr 5fr"
+          overflowY="hidden"
+        >
+          <GridItem minH="100%">
+            <Navbar />
+          </GridItem>
+          <GridItem height="100vh" overflowY="auto">
+            <main>
+              {status === "loading" ? (
+                <Flex minH="100vh" alignItems="center" justifyContent="center">
+                  <GridLoader color="#172237" size={20} />
+                </Flex>
+              ) : (
+                children
+              )}
+            </main>
+          </GridItem>
+        </Grid>
+        <AnalyticsWrapper />
+      </main>
     </>
   );
 };
